2ff06582f065e02672db3c65c21a2c1881681c2a
tedster-08 <75181949+tedster-08@users.noreply.github.com>
2021-10-24 12:48:44 -0400

    added post title field

--- website/models.py
+++ website/models.py
@@ -5,6 +5,7 @@ from sqlalchemy.sql import func
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(512))
     text = db.Column(db.String(8192))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
